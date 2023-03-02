9d19e08c35637e33b7e8d35c79f509bdfeffc7e1
praneethrms04 <praneethrms04@gmail.com>
2023-03-02 23:14:00 +0530

    countdowntimer createdt

---
+++ index.js
@@ -0,0 +1,29 @@
+document.getElementById("submit").onclick = (e) => {
+  e.preventDefault();
+
+  const countDownDate = document.getElementById("dateTime").value;
+
+  var t = setInterval(() => {
+    var now = new Date();
+
+    var distance = new Date(countDownDate).getTime() - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+    if (distance < 0) {
+      clearInterval(t);
+      document.getElementById("days").innerHTML = "00";
+      document.getElementById("hours").innerHTML = "00";
+      document.getElementById("minutes").innerHTML = "00";
+      document.getElementById("seconds").innerHTML = "00";
+    }
+  }, 1000);
+};
